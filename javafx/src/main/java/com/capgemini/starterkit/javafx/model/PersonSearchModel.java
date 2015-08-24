f43add430057bf61a1e5c5d68d67b698ff0220df
Leszek <leszek.zajac.dan@gmail.com>
2015-08-24 20:20:20 +0200

    Initial commit

---
+++ javafx/src/main/java/com/capgemini/starterkit/javafx/model/PersonSearchModel.java
@@ -0,0 +1,64 @@
+package com.capgemini.starterkit.javafx.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.capgemini.starterkit.javafx.dataprovider.data.PersonVO;
+
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+
+/**
+ * Data displayed on the person search screen.
+ *
+ * @author Leszek
+ */
+public class PersonSearchModel {
+
+	private final StringProperty name = new SimpleStringProperty();
+	private final ObjectProperty<Sex> sex = new SimpleObjectProperty<>();
+	private final ListProperty<PersonVO> result = new SimpleListProperty<>(
+			FXCollections.observableList(new ArrayList<>()));
+
+	public final String getName() {
+		return name.get();
+	}
+
+	public final void setName(String value) {
+		name.set(value);
+	}
+
+	public StringProperty nameProperty() {
+		return name;
+	}
+
+	public final Sex getSex() {
+		return sex.get();
+	}
+
+	public final void setSex(Sex value) {
+		sex.set(value);
+	}
+
+	public ObjectProperty<Sex> sexProperty() {
+		return sex;
+	}
+
+	public final List<PersonVO> getResult() {
+		return result.get();
+	}
+
+	public final void setResult(List<PersonVO> value) {
+		result.setAll(value);
+	}
+
+	public ListProperty<PersonVO> resultProperty() {
+		return result;
+	}
+
+}
